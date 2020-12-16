38c7c0087cfb5073fb55b2d4b7bc7eb792fa7953
Hassan <hassan.ahmed1991999@gmail.com>
2020-12-16 16:15:14 +0200

    [Git] First commit

---
+++ LoggerApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  LoggerApp
+//
+//  Created by khaled mohamed el morabea on 2/23/20.
+//  Copyright © 2020 Instabug. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+  
+  
+}
+
